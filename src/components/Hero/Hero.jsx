cad52866ffbab0ee86a0a8f0243728e778c49bd2
Bibek Shah <124855267+Bibekshah123@users.noreply.github.com>
2024-02-03 12:33:03 +0545

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '.Hero.css'
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className='left-h'>left side</div>
+            <div className='right-h'>right side</div>
+        </div>
+    )
+}
