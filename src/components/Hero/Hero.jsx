e3d1eab3f8a734617c7d0f3af4d2b131cdc28622
Bibek Shah <124855267+Bibekshah123@users.noreply.github.com>
2024-02-03 21:44:52 +0545

    updated files

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,6 +11,39 @@ const Hero = () => {
                     <div></div>
                     <span>the best club in the Valley</span>
                 </div>
+
+                <div className='hero-text'>
+                    <div>
+                        <span className='stroke-text'>Shape </span>
+                        <span>Your</span>
+                    </div>
+                    <div><span>Ideal Body</span>
+                    </div>
+                    <div>
+                        <span>
+                        In here we will help you to shape and build your Ideal 
+                        Body and live up your life to fullest
+                        </span>
+                    </div>
+                </div>
+                <div className='figures'>
+                    <div>
+                        <span>+140</span>
+                        <span>expert coachs</span>
+                    </div>
+                    <div>
+                        <span>+978</span>
+                        <span>members joined</span>
+                    </div>
+                    <div>
+                        <span>+50</span>
+                        <span>fitness programs</span>
+                    </div>
+                </div>
+                <div className='hero-buttons'>
+                    <buttons className="btn">Get Started</buttons>
+                    <buttons className="btn">Learn More</buttons>
+                </div>
             </div>
             <div className='right-h'>right side</div>
         </div>
