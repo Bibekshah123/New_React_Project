7a0b8826dced6350c1be2105fb505028debb9770
Bibek Shah <124855267+Bibekshah123@users.noreply.github.com>
2024-02-20 15:49:54 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Hero from './components/Hero/Hero';
 import Join from './components/Join/Join';
 import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
-import Reasons from './components/Reasons/Reasons';
+import Reasons from './components/Reasons/Reasons'; 
 function App() {
   return (
     <div className="App">
